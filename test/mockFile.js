3396cc75e575ef3ef5088dfe281b7026cadb6c0a
Ian Wang <gogoenwon@gmail.com>
2018-08-31 18:01:01 +0800

    add babel-eslint

--- test/mockFile.js
+++ test/mockFile.js
@@ -1,1 +1,10 @@
-console.log('YOYO');
+import standard from '../'
+
+export default class Hello {
+  world () {
+    const foo = () => {
+      console.log('YOYO', typeof standard);
+    }
+    foo()
+  }
+}
