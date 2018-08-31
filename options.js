658ff41fd99f011b0f56b00dafe01989488ee029
Ian Wang <gogoenwon@gmail.com>
2018-08-31 17:02:01 +0800

    init

---
+++ options.js
@@ -0,0 +1,13 @@
+var eslint = require('eslint')
+var path = require('path')
+var pkg = require('./package.json')
+
+module.exports = {
+  version: pkg.version,
+	cmd: 'lint',
+	eslint: eslint,
+	eslintConfig: {
+		configFile: path.join(__dirname, 'eslintrc.json')
+	},
+	tagline: 'Use Codementor lint Style'
+}
