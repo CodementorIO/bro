b1bc480f25d363b1387b2fec0d4e5cc883667c89
Ian Wang <gogoenwon@gmail.com>
2018-09-01 00:43:45 +0800

    update prettier rule

--- test/mockFile.js
+++ test/mockFile.js
@@ -1,8 +1,4 @@
-export default class Hello {
-  world () {
-    const foo = (e) => {
-      console.log('YOYO', typeof standard);
-    }
-    foo('qwdqwdqwd')
-  }
+const foo = (e) => {
+  console.log('YOYO', typeof standard);
 }
+foo('qwdqwdqwd')
