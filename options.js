20730b381a8368fac2864fde3a455a8821621ba0
Ian Wang <gogoenwon@gmail.com>
2019-01-08 11:01:58 +0800

    set env to jest; remove tagline

--- options.js
+++ options.js
@@ -3,12 +3,11 @@ var path = require('path')
 var pkg = require('./package.json')
 
 module.exports = {
-  version: pkg.version,
 	cmd: 'bro',
+  version: pkg.version,
   homepage: pkg.homepage,
 	eslint: eslint,
 	eslintConfig: {
 		configFile: path.join(__dirname, 'eslintrc.json')
-	},
-	tagline: 'A bro will lint and prettify your code'
+	}
 }
