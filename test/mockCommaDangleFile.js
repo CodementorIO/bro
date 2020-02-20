2e9d9308edeed67f9797377c152a1779a9aafa27
robin <almigod0718@gmail.com>
2020-02-20 17:26:37 +0800

    Add comma dangle rule

---
+++ test/mockCommaDangleFile.js
@@ -0,0 +1,17 @@
+const foo = {
+  bar: 'baz',
+  qux: 'quux'
+}
+const bar = { bar: 'baar' }
+const arrFoo = [1, 2]
+const arrBar = [
+  'the first item',
+  'the second item',
+  'the forth item',
+  'the fifth item',
+  'the sixth item'
+]
+console.log(foo)
+console.log(bar)
+console.log(arrFoo)
+console.log(arrBar)
