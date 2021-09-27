dfa7e2903835bc65ebc0f1f906108389133372be
Nate Lee <mydreamstyle@gmail.com>
2021-09-27 16:39:45 +0800

    Add stylelint

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,29 @@
 #!/usr/bin/env node
 
-var opts = require('../options.js')
+const opts = require('../options.js')
+const sgf = require("staged-git-files")
+const stylelint = require('stylelint')
+
 require('standard-engine').cli(opts)
+
+sgf((err, results) => {
+  if (err) {
+    console.error(err)
+    process.exit(2);
+  }
+
+  const stagedFiles = results
+    .filter(res => res.filename.startsWith('src/') && res.filename.endsWith('.js'))
+    .map(res => res.filename)
+
+  stylelint.lint({
+    files: stagedFiles,
+    formatter: 'string',
+    cache: true,
+  }).then(function ({ output, errored }) {
+    console.log(output);
+    if (errored) {
+      process.exit(2);
+    }
+  });
+});
